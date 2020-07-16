fa98615c7457800b5ba6fc5d49ff18c7fec60a36
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2020-07-15 23:33:26 -0300

    create initial screen

---
+++ src/components/initial-screen/initial.style.ts
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const StyledPage = styled.div`
+  height: 100%;
+  padding: 30px 40px;
+  background-color: #f8e8c9;
+`;
+
+export const StyledTitle = styled.div`
+  font-size: 80px;
+  font-weight: bold;
+  color: #1d7f35;
+`;
+
+export const CardsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+`;
+
+export const Card = styled.div`
+  width: 100%;
+  margin: 0px 10px;
+  background-color: #f7a827;
+  color: #79122f;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 15px;
+  border-radius: 3px;
+`;
--- src/pages/initial.tsx
+++ src/pages/initial.tsx
@@ -1,7 +1,43 @@
 import React from "react";
+import {
+  StyledTitle,
+  StyledPage,
+  CardsContainer,
+  Card,
+} from "../components/initial-screen/initial.style.ts";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import Text from "antd/lib/typography/Text";
+
+const TEXT_STYLE = {
+  color: "#79122f",
+  fontSize: 20,
+};
 
 const Initial: React.FC = () => {
-  return <h1>Initial screen</h1>;
+  return (
+    <StyledPage>
+      <StyledTitle>Loja de Tudo</StyledTitle>
+      <CardsContainer>
+        <Card>
+          <Text strong style={TEXT_STYLE}>
+            Já sou cliente
+          </Text>
+          <Link to="/login">
+            <Button>Entrar</Button>
+          </Link>
+        </Card>
+        <Card>
+          <Text strong style={TEXT_STYLE}>
+            Quero comprar pela primeira vez
+          </Text>
+          <Link to="/sign-up">
+            <Button>Cadastrar-se</Button>
+          </Link>
+        </Card>
+      </CardsContainer>
+    </StyledPage>
+  );
 };
 
 export default Initial;
