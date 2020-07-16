c185134fe3bf462d4b3c80f0bd56d57f60d70c2b
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2020-07-16 12:04:03 -0300

    create login screen

---
+++ src/components/login-screen/login.style.ts
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const StyledLoginTitle = styled.div`
+  color: #1d7f35;
+  font-size: 40px;
+  font-weight: bold;
+`;
+
+export const Card = styled.div`
+  margin: 30px 25% 0px;
+  background-color: #f8e8c9;
+  box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.3);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  padding: 24px 15px 0px;
+  border-radius: 3px;
+  border: gray 0.5px solid;
+`;
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
+import { StyledLoginTitle, Card } from "../components/login-screen/login.style";
+import { Form, Input, Button } from "antd";
+import { Link } from "react-router-dom";
+
+const CREDENTIALS = {
+  email: "cliente@gmail.com",
+  password: "senha",
+};
+
+const layout = {
+  labelCol: { span: 5 },
+  wrapperCol: { span: 16 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 4, span: 16 },
+};
 
 const Login: React.FC = () => {
-  return <h1>Login screen</h1>;
+  const [validEmail, setValidEmail] = useState(false);
+  const [validPassword, setValidPassword] = useState(false);
+
+  const validateEmail = (_rule: any, value: any) => {
+    setValidEmail(value === CREDENTIALS.email);
+  };
+
+  const validatePassword = (_rule: any, value: any) => {
+    setValidPassword(value === CREDENTIALS.password);
+  };
+
+  const onClick = () => {
+    if (!validEmail) {
+      alert("Email não cadastrado");
+    } else if (validEmail && !validPassword) {
+      alert("Senha inválida");
+    }
+  };
+
+  return (
+    <Fragment>
+      <StyledLoginTitle>Entrar</StyledLoginTitle>
+      <Card>
+        <Form {...layout} name="login" initialValues={{ remember: true }}>
+          <Form.Item
+            label="E-mail"
+            name="email"
+            rules={[
+              {
+                validator: validateEmail,
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ validator: validatePassword }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button htmlType="submit" onClick={onClick} danger type="primary">
+              {validEmail && validPassword ? (
+                <Link to="/products-list">Entrar</Link>
+              ) : (
+                "Entrar"
+              )}
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </Fragment>
+  );
 };
 
 export default Login;
