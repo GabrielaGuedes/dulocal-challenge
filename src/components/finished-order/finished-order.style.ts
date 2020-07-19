72313cdbd4830a79a65cc3ccf1bfc1fb68170bb6
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2020-07-18 23:09:23 -0300

    add finished order page

---
+++ src/components/finished-order/finished-order.style.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const PayedPriceTextContainer = styled.div`
+  font-size: 20px;
+  margin: 15px 0px;
+`;
--- src/pages/finished-order.tsx
+++ src/pages/finished-order.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Title } from "../components/common/title";
+import { Button } from "antd";
+import { Link } from "react-router-dom";
+import { PayedPriceTextContainer } from "../components/finished-order/finished-order.style";
 
 const FinishedOrder: React.FC = () => {
-  return <h1>Finished order screen</h1>;
+  const productsBought = JSON.parse(
+    localStorage.getItem("productsInCart") || "[]"
+  );
+  const payedPrice = productsBought
+    .reduce(
+      (sumPrice: any, product: any) => sumPrice + parseFloat(product.price),
+      0
+    )
+    .toFixed(2);
+  localStorage.clear();
+
+  return (
+    <Fragment>
+      <Title>Pedido realizado!</Title>
+      <PayedPriceTextContainer>
+        Valor total pago: R$ {payedPrice}
+      </PayedPriceTextContainer>
+      <Button danger type="primary">
+        <Link to="/products-list">Voltar para tela dos produtos</Link>
+      </Button>
+    </Fragment>
+  );
 };
 
 export default FinishedOrder;
