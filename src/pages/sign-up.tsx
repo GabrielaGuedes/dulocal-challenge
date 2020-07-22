f1455987dbfe0acdec2759fd88682bc2d5731660
Gabriela Matos Guedes <gabriela.guedes.gmg@gmail.com>
2020-07-21 21:13:45 -0300

    improve sign up page layout

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -2,82 +2,98 @@ import React, { Fragment } from "react";
 import { Title } from "../components/common/title";
 import { Form, Input, Button, DatePicker } from "antd";
 import { isCpfValid } from "../utils/cpf-validator";
+import { Card } from "../components/login-screen/login.style";
+import { useHistory } from "react-router-dom";
+
+const layout = {
+  labelCol: { span: 5 },
+  wrapperCol: { span: 20 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 4, span: 16 },
+};
 
 const SignUp: React.FC = () => {
+  const history = useHistory();
+
   return (
     <Fragment>
       <Title>Cadastre-se!</Title>
-      <Form>
-        <Form.Item
-          label="Nome"
-          name="name"
-          rules={[{ required: true, message: "Campo obrigatório" }]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="E-mail"
-          name="email"
-          rules={[
-            { type: "email", message: "E-mail inválido" },
-            { required: true, message: "Campo obrigatório" },
-          ]}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="Senha"
-          name="password"
-          rules={[{ required: true, message: "Campo obrigatório" }]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          name="password-confirmation"
-          label="Confirme a senha"
-          dependencies={["password"]}
-          rules={[
-            { required: true, message: "Campo obrigatório" },
-            ({ getFieldValue }) => ({
-              validator(_rule, value) {
-                if (!value || getFieldValue("password") === value) {
-                  return Promise.resolve();
-                }
-                return Promise.reject("As senhas digitadas são diferentes");
-              },
-            }),
-          ]}
-        >
-          <Input.Password />
-        </Form.Item>
-        <Form.Item
-          label="Data de nascimento"
-          name="birthdate"
-          rules={[{ required: true, message: "Campo obrigatório" }]}
-        >
-          <DatePicker />
-        </Form.Item>
-        <Form.Item
-          label="CPF (sem pontos ou traços, apenas números)"
-          name="cpf"
-          rules={[
-            { required: true, message: "Campo obrigatório" },
-            () => ({
-              validator(_rule, value) {
-                if (isCpfValid(value)) {
-                  return Promise.resolve();
-                }
-                return Promise.reject("CPF inválido");
-              },
-            }),
-          ]}
-        >
-          <Input type="number" />
-        </Form.Item>
-        <Form.Item>
-          <Button htmlType="submit">Realizar cadastro</Button>
-        </Form.Item>
-      </Form>
+      <Card>
+        <Form {...layout} onFinish={() => history.replace("/products-list")}>
+          <Form.Item
+            label="Nome"
+            name="name"
+            rules={[{ required: true, message: "Campo obrigatório" }]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="E-mail"
+            name="email"
+            rules={[
+              { type: "email", message: "E-mail inválido" },
+              { required: true, message: "Campo obrigatório" },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label="Senha"
+            name="password"
+            rules={[{ required: true, message: "Campo obrigatório" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            name="password-confirmation"
+            label="Confirme a senha"
+            dependencies={["password"]}
+            rules={[
+              { required: true, message: "Campo obrigatório" },
+              ({ getFieldValue }) => ({
+                validator(_rule, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject("As senhas digitadas são diferentes");
+                },
+              }),
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+          <Form.Item
+            label="Data de nascimento"
+            name="birthdate"
+            rules={[{ required: true, message: "Campo obrigatório" }]}
+          >
+            <DatePicker style={{ textAlign: "left", display: "block" }} />
+          </Form.Item>
+          <Form.Item
+            label="CPF (só números)"
+            name="cpf"
+            rules={[
+              { required: true, message: "Campo obrigatório" },
+              () => ({
+                validator(_rule, value) {
+                  if (isCpfValid(value)) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject("CPF inválido");
+                },
+              }),
+            ]}
+          >
+            <Input type="number" />
+          </Form.Item>
+          <Form.Item {...tailLayout}>
+            <Button htmlType="submit" danger type="primary">
+              Realizar cadastro
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
     </Fragment>
   );
 };
